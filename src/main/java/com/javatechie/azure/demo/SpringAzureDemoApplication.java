7d77706b4922cf959c15a8db1a22f8aa4a373c21
ashwini-vini <ashwinibaluragi@gmail.com>
2026-04-24 23:42:24 +0530

    first commit

---
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.javatechie.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! Your app deployed successfully in Azure !"
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
